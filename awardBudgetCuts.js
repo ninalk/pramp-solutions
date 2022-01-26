7d0c5b0294d67fcbc22a698116bd89912df55e6d
Nina Kim <neenerskim@gmail.com>
2022-01-25 16:25:37 -0800

    first commit to add pramp solutions

---
+++ awardBudgetCuts.js
@@ -0,0 +1,51 @@
+// Award Budget Cuts
+// The awards committee of your alma mater (i.e. your college/university) asked for your assistance 
+// with a budget allocation problem they’re facing. Originally, the committee planned to give N 
+// research grants this year. However, due to spending cutbacks, the budget was reduced to newBudget 
+// dollars and now they need to reallocate the grants. The committee made a decision that they’d 
+// like to impact as few grant recipients as possible by applying a maximum cap on all grants. 
+// Every grant initially planned to be higher than cap will now be exactly cap dollars. Grants less 
+// or equal to cap, obviously, won’t be impacted.
+
+// Given an array grantsArray of the original grants and the reduced budget newBudget, write a 
+// function findGrantsCap that finds in the most efficient manner a cap such that the least number 
+// of recipients is impacted and that the new budget constraint is met (i.e. sum of the N reallocated 
+// grants equals to newBudget).
+
+// Analyze the time and space complexities of your solution.
+
+// Example:
+// input:  grantsArray = [2, 100, 50, 120, 1000], newBudget = 190
+// output: 47 # and given this cap the new grants array would be
+//            # [2, 47, 47, 47, 47]. Notice that the sum of the
+//            # new grants is indeed 190
+
+function findGrantsCap(grantsArray, newBudget) {
+  let size = grantsArray.length;
+
+  // edge case if size is 0
+  if (size === 0) return 0;
+
+  // sort grantsArray
+  grantsArray.sort((a,b) => a - b);
+
+  let capCount = size;
+  let remainderBudget = newBudget;
+  let cap = remainderBudget / size;
+
+  // loop through grantsArray
+  for (let i = 0; i < size; i++) {
+    // if grantsArray[i] is less than cap, decrement count to cap
+    if (grantsArray[i] < cap) {
+      capCount--;
+      remainderBudget -= grantsArray[i];
+      cap = remainderBudget / capCount;
+    }
+  }
+  
+  // return most efficient cap
+  return cap;
+}
+
+// TEST EXAMPLE
+console.log(findGrantsCap([2, 100, 50, 120, 1000], 190))
---
+++ islandCount.js
@@ -0,0 +1,55 @@
+// Island Count
+// Given a 2D array binaryMatrix of 0s and 1s, implement a function getNumberOfIslands that returns 
+// the number of islands of 1s in binaryMatrix.
+
+// An island is defined as a group of adjacent values that are all 1s. A cell in binaryMatrix is 
+// considered adjacent to another cell if they are next to each either on the same row or column. 
+// Note that two values of 1 are not part of the same island if they’re sharing only a mutual 
+// “corner” (i.e. they are diagonally neighbors).
+
+// Explain and code the most efficient solution possible and analyze its time and space complexities.
+
+// Example:
+// input:  binaryMatrix = [ [0,    1,    0,    1,    0],
+//                          [0,    0,    1,    1,    1],
+//                          [1,    0,    0,    1,    0],
+//                          [0,    1,    1,    0,    0],
+//                          [1,    0,    1,    0,    1] ]
+// output: 6 # since this is the number of islands in binaryMatrix.
+//           # See all 6 islands color-coded below.
+
+function getNumberOfIslands(binaryMatrix) {
+  let row = binaryMatrix.length,
+    col = binaryMatrix[0].length,
+    islandCount = 0;
+
+  // loop through binaryMatrix
+  for (let r = 0; r < row; r++) {
+    for (let c = 0; c < col; c++) {
+      if (binaryMatrix[r][c] === 1) {
+        islandCount++;
+        checkNeighbors(r, c, binaryMatrix);
+      }
+    }
+  }
+
+  return islandCount;
+}
+
+function checkNeighbors(row, col, matrix) {
+  // check boundaries
+  if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) {
+    return;
+  }
+
+  // check neighbors top, bottom, left, and right of current val
+  const curr = matrix[row][col];
+  
+  if (curr === 1) {
+    matrix[row][col] = -1;
+    checkNeighbors(row - 1, col, matrix) // check top
+    checkNeighbors(row + 1, col, matrix) // check bottom
+    checkNeighbors(row, col - 1, matrix) // check left
+    checkNeighbors(row, col + 1, matrix) // check right
+  }
+}
